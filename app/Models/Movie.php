fe887dcc5b8f4c469250525804ade55478d1034f
MatteoNocera <matteo_nocera@tiscali.it>
2023-10-27 17:41:58 +0200

    change Model from laravel..ecc in Movie connect db

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class laravel-model-controller extends Model
+class Movie extends Model
 {
     use HasFactory;
 }
